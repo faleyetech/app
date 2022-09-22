372b4cbf41111a9d1915d8a0f709c2baa53cd3e5
o.tech <kratosolomon25@gmail.com>
2022-09-22 18:27:55 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,207 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import Logo  from "./assets/images/mm3d.png";
+import rightImage from "./assets/images/bear.png";
+import { FaBurn } from 'react-icons/fa';
+import Accordion from "./Accordion";
+import { Howl } from 'howler';
+  import { ToastContainer, toast } from 'react-toastify';
+    import 'react-toastify/dist/ReactToastify.css';
+// import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import { useEffect, useRef, useState } from "react";
+import LUCK from './data'
+import Image11 from "./assets/images/logo1.jpg";
+import Image12 from "./assets/images/logo2.jpg";
+import Image13 from "./assets/images/logo3.jpg";
+import Image14 from "./assets/images/logo4.jpg";
+import Image15 from "./assets/images/logo5.jpg";
+import Image16 from "./assets/images/logo6.jpg";
+import Image17 from "./assets/images/logo7.jpg";
+import Image18 from "./assets/images/logo8.jpg";
+import Image19 from "./assets/images/bear.png";
+
+
+// constructor(){
+//     super();
+//     this.state ={
+//       sport:''
+//     };
+// };
+
+// pickIt=['You are correct', "Sorry Try Again Next Time","Congratulation's You Won", "wow you almost there try again"];
+
+// componetDidMount(){
+//   roundPick(()=>{
+//     this.setState({sport: this.roundPick[Math.floor(Math.random()*this.roundPick.length)]});
+//   })
+// }
+
+
+const imagesList = [
+  Image11,
+  Image12,
+  Image13,
+  Image14,
+  Image15,
+  Image16,
+  Image17,
+  Image18,
+  Image19,
+];
 
 function App() {
+const soundSrc="https://assets.mixkit.co/sfx/download/mixkit-medium-size-angry-dog-bark-54.wav"
+const soundDog="https://assets.mixkit.co/sfx/download/mixkit-dog-whimper-sad-466.wav"
+const [luck,  setLuck ]=useState("");
+
+const getLuck =()=>{
+  // fetch('https://type.fit/api/quotes')
+  // .then((res)=>res.json())
+  // .then((data)=>{
+    let randomNum = Math.floor(Math.random()* LUCK.length);
+
+    setLuck(LUCK[randomNum]);
+  // });
+};
+
+  const horizontalScroll = useRef(null);
+
+  useEffect(() => {
+    setInterval(() => {
+      if (horizontalScroll.current !== null) {
+        horizontalScroll.current.scrollBy({
+          left: 1
+        });
+
+        // if we scrolled end
+        if (horizontalScroll.current.scrollLeft === horizontalScroll.current.scrollWidth - horizontalScroll.current.clientWidth) {
+          horizontalScroll.current.scrollTo({
+            left: 0
+          });
+        }
+      }
+    }, 40);
+  }, []);
+
+const notify = () => toast(luck.text,
+  {
+position: "top-right",
+autoClose: 5000,
+hideProgressBar: false,
+closeOnClick: true,
+pauseOnHover: true,
+draggable: true,
+progress: undefined,
+}
+  );
+  const callMySound =(src)=>{
+    const sound =new Howl({
+      src,
+      html5:true,
+    });
+    sound.play();
+  };
+   const callSound =(src)=>{
+    const dog =new Howl({
+      src,
+      html5:true,
+    });
+    dog.play();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section id="header-section">
+        <div className="top-bar">
+          <div className="button">
+            Wallet connect
+          </div>
+
+         
+        </div>
+
+        <div className="middle-content">
+        <img alt="" src={Logo}  className="logo" />
+          
+          
+           <h1>MOON MONGRELZ</h1>
+          <h3> Buy,Sell and Discover Extraordinary NFT<br />
+Find the rarest of the art you can find in NFT world the sooner you place the bid the sooner you get the hands on these Extraordinary NFT's <br />
+To commemorate the ongoing achievement of this amazing blockchain and eco-system, we bring you this special collection of 1500 art pieces.
+          </h3>
+          {/* <div className="mint-button">
+            Mint now
+          </div> */}
+        
+          <button onClick={()=>{
+            callMySound(soundSrc)
+          
+           }}><span>MINT</span></button>
+        </div>
+
+        <div className="horizontal-scroll" ref={horizontalScroll}>
+          {imagesList.map((item, index) => <div key={index} className="item">
+            <img alt=""  src={item}  />
+          </div>)}
+        </div>
+      </section>
+
+      <section id="second-section">
+        <div className="left-content">
+          <h1>
+            MOON MONGRELZ
+          </h1>
+          <h4>Extraordinary NFT</h4>
+          <h6>
+            Find the rarest of the art you can find in NFT world the sooner you place the bid the sooner you get the hands on these Extraordinary NFT's
+
+To commemorate the ongoing achievement of this amazing blockchain and eco-system, we bring you this special collection of 1500 art pieces.
+
+
+          </h6>
+        </div>
+        <div className="right-content">
+          <img alt="" src={rightImage}  />
+        </div>
+      </section>
+
+      <div className="card">
+<div className="img"><FaBurn className="icon" /></div>
+  <div className="info">
+    <span>WARNING!!!</span>
+    <p style={{ color: '#cce0f3'}} > If you use this feature, it cannot be undone and your pooch will be gone FOREVER
+
+</p>
+  </div>
+ <nav  onClick={()=>{callSound(soundDog)  
+  getLuck()  
+ notify() }}> BURN</nav>
+
+</div>
+<div className="mfaq">
+  <h1>FAQ</h1>
+<div className="faq">
+  <Accordion/>
+</div>
+</div>
+      <footer>
+       
+        <h6>Copyright © {new Date().getFullYear()}</h6>
+      </footer>
+      <ToastContainer
+      position="top-right"
+autoClose={5000}
+hideProgressBar={false}
+newestOnTop={false}
+closeOnClick
+rtl={false}
+pauseOnFocusLoss
+draggable
+pauseOnHover
+      />
     </div>
+
   );
 }
 
---
+++ src/Pick.js
@@ -0,0 +1,30 @@
+export const Pick = [
+  {
+    question: 'How much rewards are there?',
+    answer: '300 random dogs have an 850VLX reward attached. That is 20% of the total supply and a total of 255,000VLX'
+  },
+  {
+    question: 'How do I claim my reward?',
+    answer: 'Not every dog has the reward attached, but if you do win, you will see the confirmation on screen when you burn, and the 850 VLX is automatically transferred to your wallet during the process. In short, you don’t need to do anything, it’s automatic. The current stats are transparently displayed on the website'
+  },
+  {
+    question: 'Who are the team behind the project?',
+    answer: 'I go by the name BlockApprentice, this is a personal solo project of mine. You may have seen me in the community. I am also one of the founders of Velas.Army'
+  },
+  {
+    question: 'Where did the idea come from?',
+    answer: 'I was looking at ideas for a project and studying tokenomics of various projects. Watching charts all day, it fascinated me to see the behaviour of different traders buying and selling. With this concept I thought it interesting to watch who will hold and who will burn, in this project both can win due to the decrease in the supply, and having the reward directly attached to the NFT. So holding for VLX price to increase is like a store of value.'
+  },
+  {
+    question: 'What about the inspiration behind the art?',
+    answer: 'That is easy. As an active community member I watched the eco system develop, so I wanted a way to capture the Velas blockchain like a snapshot in time in my traits. The idea of the dog, is my very own pet boxer, called Bella. We both was at home one day thinking which trades we will make to get to the moon, that’s the reason for the name and the jet pack.'
+  },
+  {
+    question: 'Why are you releasing now in the current market when many projects are on hold?',
+    answer: 'I think of it as a gift. The project has been in development for some time. To buy a MoonMongrel when Velas is at a low price will mean potentially a greater value of profit if you were to win on a burn in the future with Velas at a higher price. This is of course down to speculation. Me as a strong Velas supporter will be HODLING any revenue either way so it makes no difference for me to release the project now or later'
+  },
+  {
+    question: 'When a Mongrel is burnt, where does it go?',
+    answer: 'The NFT is sent to a contract address and unretrievable. They will never be put back into circulation'
+  }
+];
